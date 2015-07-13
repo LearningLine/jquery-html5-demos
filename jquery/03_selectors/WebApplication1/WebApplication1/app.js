7879e674d3e2ab70075cf80c3695946187cc80ee
Brock Allen <brockallen@gmail.com>
2015-07-13 18:01:08 -0400

    selectors

---
+++ jquery/03_selectors/WebApplication1/WebApplication1/app.js
@@ -0,0 +1,70 @@
+﻿/// <reference path='jquery.js' />
+
+$(function () {
+
+    //$(".female").css("color", "red");
+    //$("span.female").css("color", "red");
+    //$(".female.mean").css("color", "red");
+    //$("li#brock").css("color", "red");
+    //$("#people .female").css("color", "red");
+    //$("#people > .female").css("color", "red");
+    //$("#people > * > .female").css("color", "red");
+    //$("li[title]").css("color", "red");
+    //$("li[title = 'Nice guy']").css("color", "red");
+    //$("li[title ^= 'Nice']").css("color", "red");
+    //$("li[title $= 'gal']").css("color", "red");
+    //$("li[title $= 'gal'], li[title$=guy]").css("color", "red");
+    //$("li[title ~= 'not']").css("color", "red");
+    //$("li.female + li").css("color", "red");
+    //$("li.female ~ li").css("color", "red");
+    //$("li.female ~ li").css("color", "red");
+    //$("li:first").css("color", "red");
+    //$("li:last").css("color", "red");
+    //$("li:even").css("color", "red");
+    //$("li:nth-child(2)").css("color", "red");
+    //$("li:nth-child)(3n-1").css("color", "red");
+    //$("li:lt(2)").css("color", "red");
+    //$("li:not(.female)").css("color", "red");
+    //$("li:has(span)").css("color", "red");
+    //$("li:contains(s)").css("color", "red");
+
+    //$.expr[":"].foo = function (elem, index, ) {
+    //};
+
+
+    //var list = $("jkldfhjdfk");
+    //if (list.length) {
+    //}
+
+    //$("li").each(function (index, elem) {
+    //    console.log(index, elem);
+    //});
+
+    //var new_list = $("li").filter(function (index, elem) {
+    //    console.log(index, elem);
+    //    return index % 3 === 0;
+    //    //elem.attributes["title"].toUpper()
+    //});
+    //console.log(new_list);
+
+    //$("li")
+    //    .css('color', 'red')
+    //    .filter(".female")
+    //        .css('fontStyle', 'italic')
+    //        .end()
+    //    .filter(":not(.female)")
+    //        .css('fontWeight', 'bold');
+
+
+    //$("li")
+    //    .css('color', 'red')
+    //    .find("span")
+    //        .css('color', 'green');
+
+    //$("span.female")
+    //    .css('color', 'red')
+    //    .closest("li")
+    //        .css("backgroundColor", "pink");
+
+    //$("li")[0]
+});
