446ef92a415037a0fdb7df98a36f08fb771113cd
Brock Allen <brockallen@gmail.com>
2015-07-21 12:38:15 -0400

    background workers

---
+++ html5/04_workers/WebApplication1/WebApplication1/work.js
@@ -0,0 +1,12 @@
+﻿
+onmessage = function (e) {
+    if (e.data.command === "start") {
+        var result = 0;
+
+        for (var i = 0; i < 9999999; i++) {
+            result += i;
+        }
+
+        postMessage({ status: "done", result: result });
+    }
+}
