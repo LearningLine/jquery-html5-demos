3f9a4c6cd3848db9a468c2f4058995702136f201
Brock Allen <brockallen@gmail.com>
2015-07-21 15:15:57 -0400

    networking

---
+++ html5/05_networking/WebApplication1/WebApplication1/Chat.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Chat : Hub
+    {
+        public override Task OnConnected()
+        {
+            //this.Context.ConnectionId
+            //this.Context.User
+
+            this.Groups.Add(this.Context.ConnectionId, "Managers");
+
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            return base.OnReconnected();
+        }
+
+        public void SendMessage(string message)
+        {
+            //this.Context.ConnectionId
+            //this.Clients.Client("123")
+            this.Clients.All.GotMessage(message);
+            //this.Clients.AllExcept()
+            //this.Clients.Caller
+            //this.Clients.Caller.Foo();
+            //this.Clients.Group("Managers").GotMessage("whatever");
+
+            //dim x = new Foo()
+            //x.Foo()
+        }
+    }
+}
---
+++ html5/05_networking/WebApplication1/WebApplication1/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.SignalR;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+
+            Task.Factory.StartNew(async () =>
+            {
+                while(true)
+                {
+                    await Task.Delay(2000);
+                    GlobalHost.ConnectionManager.GetHubContext<Chat>()
+                        .Clients.All.GotMessage("spam! " + DateTime.Now.ToString());
+                }
+            });
+        }
+    }
+}
